e44a77ace1b7612a9783ead2b087c759619cb8d5
Kenny Lao <kennylao@Kennys-MacBook-Pro.local>
2024-08-12 20:49:24 -0700

    updates

--- utils.py
+++ utils.py
@@ -10,11 +10,7 @@ from pathlib import Path
 def setup_logging() -> logging.Logger:
     # Set up the logger with pertyyy Rich logging
 
-    # Trying to prevent logs from being squished af in Docker environments
-    terminal_size = os.get_terminal_size()
-    width = terminal_size.columns if terminal_size.columns != 0 else None
-
-    rich_console = Console(stderr=True, width=width, theme=Theme({
+    rich_console = Console(stderr=True, theme=Theme({
         "log.time": "dim magenta",
         "logging.level.debug": "bold cyan1",
         "logging.level.info": "bold cyan1",
