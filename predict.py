a94fd84a6cdc13ba285cbb5d492f0214e268d252
Kenny Lao <kennylao@Kennys-MacBook-Pro.local>
2024-08-13 17:13:09 -0700

    fucking replicate

--- predict.py
+++ predict.py
@@ -7,14 +7,13 @@ import zipfile
 from concept import train_sdxl, setup_parser
 import mimetypes
 
-mimetypes.add_type("application/octet-stream", ".safetensors")
-
 
 class Predictor(BasePredictor):
     def setup(self):
         # Set up goes here
 
         print("Starting setup.")
+        mimetypes.add_type("application/octet-stream", ".safetensors")
 
     def predict(
             self,
@@ -23,29 +22,33 @@ class Predictor(BasePredictor):
     ) -> Path:
         # Run model training
 
-        # Extract zipped training data contents into a temp directory
-        train_data_dir = tempfile.mkdtemp()
-        with zipfile.ZipFile(train_data_zip, 'r') as zip_ref:
-            zip_ref.extractall(train_data_dir)
+        try:
+            # Extract zipped training data contents into a temp directory
+            train_data_dir = tempfile.mkdtemp()
+            with zipfile.ZipFile(train_data_zip, 'r') as zip_ref:
+                zip_ref.extractall(train_data_dir)
 
-        # Create output directories
-        output_dir = tempfile.mkdtemp()
-        output_tensors = Path(output_dir).joinpath("oberg_dreambooth.safetensors")
-        output_zip = Path(output_dir).joinpath("oberg_dreambooth.zip")
+            # Create output directories
+            output_dir = tempfile.mkdtemp()
+            output_tensors = Path(output_dir).joinpath("oberg_dreambooth.safetensors")
+            output_zip = Path(output_dir).joinpath("oberg_dreambooth.zip")
 
-        # Set up parser
-        parser = setup_parser()
-        args = parser.parse_args()
+            # Set up parser
+            parser = setup_parser()
+            args = parser.parse_args()
 
-        args.json_config = json_config
-        args.train_data_zip = train_data_zip
-        args.output_dir = output_dir
+            args.json_config = json_config
+            args.train_data_zip = train_data_zip
+            args.output_dir = output_dir
 
-        # Run training
-        train_sdxl(args=args)
+            # Run training
+            train_sdxl(args=args)
 
-        # Zip the safetensors file
-        with zipfile.ZipFile(output_zip, "w") as zip_write:
-            zip_write.write(output_tensors)
+            # Zip the safetensors file
+            with zipfile.ZipFile(output_zip, "w") as zip_write:
+                zip_write.write(output_tensors)
 
-        return output_zip
+            return output_zip
+
+        except Exception as e:
+            print(f"Error in predict function: {e}")
