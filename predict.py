bb03211a5cb2e643e4ed4b0a4b33af0e968f85b5
Kenny Lao <kennylao@Kennys-MacBook-Pro.local>
2024-08-14 19:30:43 -0700

    idk man

--- predict.py
+++ predict.py
@@ -1,5 +1,6 @@
 # Prediction interface for Cog ⚙️
 # https://cog.run/python
+import time
 
 from cog import BasePredictor, Input, Path as cogPath
 from pathlib import Path
@@ -7,14 +8,22 @@ import tempfile
 import zipfile
 from concept import train_sdxl, setup_parser
 import mimetypes
+import torch
+from subprocess import check_call
+from argparse import Namespace
 
 
 class Predictor(BasePredictor):
     def setup(self):
-        # Set up goes here
+        # Wait a little bit for instance to be ready and run set up
 
         print("Starting setup.")
-        mimetypes.add_type("application/octet-stream", ".safetensors")
+
+        time.sleep(10)
+        check_call("nvidia-smi", shell=True)
+        assert torch.cuda.is_available()
+
+        # mimetypes.add_type("application/octet-stream", ".safetensors")
 
     def predict(
             self,
@@ -24,44 +33,53 @@ class Predictor(BasePredictor):
         # Run model training
 
         print("Starting model training nigga.")
+
+        # Extract zipped training data contents into a temp directory
+        print("Extracting zip file contents into a temp dir.")
+        train_data_dir = tempfile.mkdtemp()
+        with zipfile.ZipFile(train_data_zip, 'r') as zip_ref:
+            zip_ref.extractall(train_data_dir)
+
+        # Create output directories
+        print("Creating the output dirs.")
+        output_dir = tempfile.mkdtemp()
+
+        # Set up parser
+        # print("Setting up the parsers.")
+        # parser = setup_parser()
+        # print("Parsing args.")
+        # args = parser.parse_args()
+        # print("Done parsing args.")
+
+        # print("Assigning arguments to parsers.")
+        # args.json_config = json_config
+        # args.train_data_zip = train_data_zip
+        # args.output_dir = output_dir
+
+        print("Assigning args to Namespace.")
+        args = {
+            "json_config": json_config,
+            "train_data_zip": train_data_zip,
+            "output_dir": output_dir
+        }
+
+        args = Namespace(**args)
+
+        # Run training
         try:
-            # Extract zipped training data contents into a temp directory
-            print("Extracting zip file contents into a temp dir.")
-            train_data_dir = tempfile.mkdtemp()
-            with zipfile.ZipFile(train_data_zip, 'r') as zip_ref:
-                zip_ref.extractall(train_data_dir)
-
-            # Create output directories
-            print("Creating the output dirs.")
-            output_dir = tempfile.mkdtemp()
-
-            # Set up parser
-            print("Setting up the parsers.")
-            parser = setup_parser()
-            print("Parsing args.")
-            args = parser.parse_args()
-            print("Done parsing args.")
-
-            print("Assigning arguments to parsers.")
-            args.json_config = json_config
-            args.train_data_zip = train_data_zip
-            args.output_dir = output_dir
-
-            # Run training
             print("Running training.")
             train_sdxl(args=args)
-
-            print("Adding path to safetensors.")
-            output_tensors = Path(output_dir).joinpath("oberg_dreambooth.safetensors")
-            print("Adding path to zip file.")
-            output_zip = Path(output_dir).joinpath("oberg_dreambooth.zip")
-
-            # Zip the safetensors file
-            print("Zipping safetensors file.")
-            with zipfile.ZipFile(output_zip, "w") as zip_write:
-                zip_write.write(output_tensors)
-
-            return output_zip
-
         except Exception as e:
-            print(f"Error in predict function: {e}")
+            print(f"An exception occured when running training script: {e}")
+
+        print("Adding path to safetensors.")
+        output_tensors = Path(output_dir).joinpath("oberg_dreambooth.safetensors")
+        print("Adding path to zip file.")
+        output_zip = Path(output_dir).joinpath("oberg_dreambooth.zip")
+
+        # Zip the safetensors file
+        print("Zipping safetensors file.")
+        with zipfile.ZipFile(output_zip, "w") as zip_write:
+            zip_write.write(output_tensors)
+
+        return output_zip
