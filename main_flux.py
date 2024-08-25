533f30de36030f8cf5360597b7c8fb535d1a5f2b
Kenski Lao <kennylao@Kennys-MacBook-Pro.local>
2024-08-25 13:53:13 -0700

    ook now ready

--- main_flux.py
+++ main_flux.py
@@ -1,6 +1,6 @@
 import argparse
 import sys
-from utils import setup_logging, BASE_FLUX_DEV_MODEL_NAME, are_models_verified_flux
+from utils import setup_logging, BASE_FLUX_DEV_MODEL_NAME, are_models_verified_flux, BASE_FLUX_DEV_CLIP_NAME, BASE_FLUX_DEV_T5_NAME, BASE_FLUX_DEV_AE_NAME
 from pathlib import Path
 import tempfile
 import zipfile
@@ -22,7 +22,12 @@ def setup_parser() -> argparse.ArgumentParser:
     parser.add_argument("--session_name", default=None, required=True, help="Name of this training session (Will appear as file names).")
     parser.add_argument("--training_data_zip", default=None, required=True, help="Path of training data in zip format.")
     parser.add_argument("--output_dir", default=None, required=True, help="Path to the local output directory.")
-    parser.add_argument("--flux_config", default=None, required=True, help="Configuration JSON file for Flux training.")
+
+    # Automatically set, but can be user-defined in the CLI.
+    # parser.add_argument("--flux_config", default=None, required=True, help="Configuration JSON file for Flux training.")
+    # parser.add_argument("--clip_l", default=None, required=True, help="Path to the clip_l model.")
+    # parser.add_argument("--t5xxl", default=None, required=True, help="Path to the t5xxl model.")
+    # parser.add_argument("--ae", default=None, required=True, help="Path to the ae model.")
 
     return parser
 
@@ -31,9 +36,9 @@ def train_flux(args: argparse.Namespace) -> None:
     # Begin training of the Flux model Lora.
 
     # Create appropriate paths to files.
-    path_to_base_flux_model = script_dir.joinpath("models", "flux_base_models", BASE_FLUX_DEV_MODEL_NAME)
     path_to_script = script_dir.joinpath("sd_scripts_flux", "flux_train_network.py")
     path_to_accelerate_config = script_dir.joinpath("configs", "accelerate.yaml")
+    path_to_flux_config = script_dir.joinpath("configs", "flux_lora.json")
 
     # Unzip file and store in temp directory.
     temp_train_dir = tempfile.mkdtemp()
@@ -56,19 +61,26 @@ def train_flux(args: argparse.Namespace) -> None:
     # run_cmd.append("bf16")
 
     # Add TOML config argument.
-    toml_config_path = begin_json_config(args.flux_config)
+    toml_config_path = begin_json_config(str(path_to_flux_config))
     run_cmd.append("--config_file")
     run_cmd.append(toml_config_path)
 
     # Add extra Flux script arguments.
     run_cmd.append("--output_name")
     run_cmd.append(f"{args.session_name}_flux_lora")
-    run_cmd.append("--pretrained_model_name_or_path")
-    run_cmd.append(str(path_to_base_flux_model))
     run_cmd.append("--train_data_dir")
     run_cmd.append(temp_train_dir)
     run_cmd.append("--output_dir")
     run_cmd.append(args.output_dir)
+
+    run_cmd.append("--pretrained_model_name_or_path")
+    run_cmd.append(str(script_dir.joinpath("models", "flux_base_models", BASE_FLUX_DEV_MODEL_NAME)))
+    run_cmd.append("--clip_l")
+    run_cmd.append(str(script_dir.joinpath("models", "flux_base_models", BASE_FLUX_DEV_CLIP_NAME)))
+    run_cmd.append("--t5xxl")
+    run_cmd.append(str(script_dir.joinpath("models", "flux_base_models", BASE_FLUX_DEV_T5_NAME)))
+    run_cmd.append("--ae")
+    run_cmd.append(str(script_dir.joinpath("models", "flux_base_models", BASE_FLUX_DEV_AE_NAME)))
 
     # Execute the command.
     executed_subprocess = execute_cmd(run_cmd=run_cmd)
